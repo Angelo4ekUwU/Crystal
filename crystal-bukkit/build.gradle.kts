8005dbffd2eab873d566de08fcd224f986f1cfb6
DenaryDev <denaryplanet@gmail.com>
2022-06-30 01:45:06 +0500

    1.19 support

--- crystal-bukkit/build.gradle.kts
+++ crystal-bukkit/build.gradle.kts
@@ -10,15 +10,15 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.sapphiremc.sapphire:sapphire-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("io.sapphiremc.sapphire:sapphire-api:1.19-R0.1-SNAPSHOT")
     compileOnly("net.luckperms:api:5.4")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("io.sapphiremc.gold:gold-api:1.3.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.1")
     compileOnly("me.filoghost.holographicdisplays:holographicdisplays-legacy-api-v2:3.0.0-SNAPSHOT")
-    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.10")
-    compileOnly(files(rootProject.projectDir.resolve("libs/CMI9.0.0.0API.jar")))
-    compileOnly(files(rootProject.projectDir.resolve("libs/CMILib1.1.2.5.jar")))
+    compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.2.11-beta-01")
+    compileOnly(files(rootProject.projectDir.resolve("libs/cmi-api.jar")))
+    compileOnly(files(rootProject.projectDir.resolve("libs/cmi-lib.jar")))
 
     implementation("mysql:mysql-connector-java:8.0.29")
     implementation("com.zaxxer:HikariCP:5.0.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,4 @@
 rootProject.name = "crystal-parent"
 
 include("crystal-bukkit")
+include("crystal-config")

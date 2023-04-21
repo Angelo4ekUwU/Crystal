8438ab7b672accc5747ea42d2ca242fffb4c767b
DenaryDev <denaryplanet@gmail.com>
2023-04-22 00:37:13 +0500

    Update GitHub workflows

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,5 @@
-plugins {
-    `java-library`
-    `maven-publish`
-}
-
-allprojects {
-    group = "me.denarydev"
-    version = "2.0.0" + getVersionInfo()
-}
-
-publishing {
-    publications.create<MavenPublication>("maven") {
-        pom.withXml {
-            val depsNode = asNode().appendNode("dependencies")
-
-            rootProject.subprojects.stream().filter {
-                it.path.split(":").size == 2
-            }.forEach {
-                val depNode = depsNode.appendNode("dependency")
-                depNode.appendNode("groupId", it.group)
-                depNode.appendNode("artifactId", it.name)
-                depNode.appendNode("version", it.version)
-                depNode.appendNode("scope", "compile")
-            }
-        }
+tasks.register("printVersion") {
+    doFirst {
+        println(version)
     }
-}
-
-fun getVersionInfo(): String {
-    if (!System.getenv("CI").isNullOrEmpty()) {
-        return "-SNAPSHOT"
-    }
-
-    if (!System.getenv("RELEASE").isNullOrEmpty()) {
-        return ""
-    }
-
-    return "-local"
 }
--- buildSrc/src/main/kotlin/crystal.library.gradle.kts
+++ buildSrc/src/main/kotlin/crystal.library.gradle.kts
@@ -6,11 +6,6 @@ plugins {
 
 val extension = project.extensions.create("crystalLibrary", CrystalLibraryExtension::class, project)
 
-// We seem to have to use afterEvaluate to verify this stuff
-afterEvaluate {
-    // TODO: Validate the extension parts
-}
-
 java {
     withSourcesJar()
     withJavadocJar()
@@ -23,34 +18,8 @@ afterEvaluate {
         it.plugins.apply("crystal.module")
     }
 
-    publishing {
-        publications.create<MavenPublication>("maven") {
-            pom.withXml {
-                val depsNode = asNode().appendNode("dependencies")
-
-                project.subprojects.stream().filter {
-                    it.path.split(":").size == 3
-                }.forEach {
-                    val depNode = depsNode.appendNode("dependency")
-                    depNode.appendNode("groupId", it.group)
-                    depNode.appendNode("artifactId", it.name)
-                    depNode.appendNode("version", it.version)
-                    depNode.appendNode("scope", "compile")
-                }
-            }
-        }
-    }
-
     // Print the version and throw an exception if it doesn't match
     if (version != rootProject.version) {
         throw GradleException("Library ${extension.libraryName.get()} version ($version) does not match root project version ($rootProject.version). Do not change it!")
-    }
-}
-
-tasks {
-    // A library isn't truly a distributed artifact, rather it is just maven metadata to depend on the modules of a library.
-    // For that reason, we have no use for the jar or remapJar tasks.
-    jar {
-        enabled = false
     }
 }
